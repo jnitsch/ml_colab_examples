78845d9b6e6ea8afbebae2bcb8b8d70cc77a25b5
Julia Nitsch <julia.nitsch@gmail.com>
2020-07-11 19:02:49 +0200

    example checkin with ipynb

--- mnist_fashion.py
+++ mnist_fashion.py
@@ -1,3 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.5.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
 # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
@@ -6,7 +22,11 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 print(tf.__version__)
+# -
 
 # Load Data Set
 fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+# Train shape
+train_images.shape
